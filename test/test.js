254ed8c02a8770d20aaa1c5e4de2fb992dca01a0
Gregor Gilchrist <Gregor Gilchrist@PDE-TEST-101110>
2019-12-06 15:11:13 +0000

    removed test step

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 // Simple async test for HTTP 200 response code using supertest
 'use strict';
 
-var request = require("supertest"),
-    app = require("../app").getApp;
+var request = require("supertest");
+var app = require("../app").getApp;
 
 describe('GET /', function(){
   it('expects HTTP response 200', function(done){
