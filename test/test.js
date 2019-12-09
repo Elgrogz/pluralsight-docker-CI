313b17a461e7ede3581fff2a3012e1f1a026fa21
Gregor Gilchrist <Gregor Gilchrist@PDE-TEST-101110>
2019-12-09 16:09:49 +0000

    added console message

--- test/test.js
+++ test/test.js
@@ -6,10 +6,12 @@ const { expect } = chai;
 chai.use(chaiHttp);
 describe("Server!", () => {
   it("welcomes user to the api", done => {
+    console.log("starting test")
     chai
       .request(app)
       .get("/")
       .end((err, res) => {
+        console.log("testing status");
         expect(res).to.have.status(200);
         done();
       });
